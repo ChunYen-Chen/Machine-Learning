0e0be9d3b396f0f91a409f5d45b004cf1ca0ec56
ChunYen-Chen <chunyenchen2019@gmail.com>
2021-12-21 18:13:15 +0800

    Add a E_out error measurement function.

--- FinalProject/Error.py
+++ FinalProject/Error.py
@@ -17,4 +17,19 @@ def Error( x, y, w, mode ):
     else:
         print("Wrong error calculate mode.")
         exit()
-    return error 
+    return error
+
+def Error_withTrue( y, y_true, mode ):
+    if mode == ERROR_BIN:
+        N = x.shape[0]
+        error = ( y * x.dot(w) < 0 ).sum()
+        error /= N
+    elif mode == ERROR_SQR:
+        error = np.mean( np.square( y_true - y ) )
+    elif mode == ERROR_CE:
+        print("Not supported mode yet.")
+        error = 0.0
+    else:
+        print("Wrong error calculate mode.")
+        exit()
+    return error
